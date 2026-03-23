c872c7fa9c3462a79c93e4d062f4358d74e2f81b
Gabriel <gabrieltmachado87@gmail.com>
2026-03-23 00:10:02 -0300

    PC

--- Componentes/Prototipo - Sprint 0/prototipoinicial.c
+++ Componentes/Prototipo - Sprint 0/prototipoinicial.c
@@ -14,8 +14,12 @@ typedef struct mem{
 
 int contaLinhas(char *arq);
 void lerArquivo(char *arq, code **memoria, int linhas);
+void programCounter(code *memoria, int linhas, int esc);
 
 int main(){
+
+    int esc;
+
     char arq[20];
     printf("Digite o nome do arquivo de memória: ");
     fgets(arq, sizeof(arq), stdin);
@@ -26,7 +30,12 @@ int main(){
 
     code *memoria = NULL;
     lerArquivo(arq, &memoria, linhas);
-    free(memoria);
+
+    printf("\n\n0 - Rodar automaticamente\n1 - Rodar passo a passo\n");
+    scanf("%d", &esc);
+    getchar();
+
+    programCounter(memoria, linhas, esc);
     return 0;
 }
 
@@ -48,7 +57,7 @@ int contaLinhas(char *arq){
 }
 
 void lerArquivo(char *arq, code **memoria, int linhas){
-    *memoria = (code *)malloc(linhas * sizeof(code));
+    *memoria = (code *)malloc(256 * sizeof(code));
     arquivo = fopen(arq, "r");
     int i=0;
     char mem[17];
@@ -64,4 +73,20 @@ void lerArquivo(char *arq, code **memoria, int linhas){
         }
     }
     fclose(arquivo);
+}
+
+void programCounter(code *memoria, int linhas, int esc){
+
+    int pc = 0;
+
+    while(pc < linhas){
+        printf("\nPC = %d | Memória = %s\n", pc, memoria[pc].mem);
+
+
+        if(esc == 1){
+            printf("Pressione enter para o próximo passo\n\n");
+            getchar();
+        }
+        pc++;
+    }
 }
