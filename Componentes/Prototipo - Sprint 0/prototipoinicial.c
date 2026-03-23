c8251e04307826878a850d180b3120f055a1fa79
Gabriel <gabrieltmachado87@gmail.com>
2026-03-23 02:48:24 -0300

    mudança do nome da struct (code -> instrucao) e inicio da decoficação das instruções (Gabriel)

--- Componentes/Prototipo - Sprint 0/prototipoinicial.c
+++ Componentes/Prototipo - Sprint 0/prototipoinicial.c
@@ -1,20 +1,54 @@
-// Atualmente lê o que está escrito em memoria1.mem e imprime na tela.
-// Lê os bits como char.
+// Decodificação em andamento (Gabriel);
+
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 FILE* arquivo;
 
-typedef struct mem{
+enum inst{
+    tipoI, tipoJ, tipoR
+};
+
+typedef struct {
     char mem[17];
-} code;
+    enum inst tipoInst;
+    uint16_t instrucao;
+    uint16_t opcode;
+    uint16_t rs;
+    uint16_t rt;
+    uint16_t rd;
+    uint16_t funct;
+    int16_t imm;
+    uint16_t addr;
+} instrucao;
+
+/*Instruções:
+Tipo R:
+opcode: 4 bits (0000);
+rs: 3 bits;
+rt: 3 bits;
+rd: 3 bits;
+funct: 3 bits
+
+Tipo I:
+opcode: 4 bits;
+rs (base/registrador fonte): 3 bits;
+rt (destino/registrador fonte): 3 bits;
+imediato: 6 bits (estendido);
+
+Tipo J:
+opcode: 4 bits;
+End: 8 bits;
+*/
 
 
 int contaLinhas(char *arq);
-void lerArquivo(char *arq, code **memoria, int linhas);
-void programCounter(code *memoria, int linhas, int esc);
+void lerArquivo(char *arq, instrucao **memoria, int linhas);
+void programCounter(instrucao *memoria, int linhas, int esc);
+void decodificaInst(instrucao *instrucao);
 
 int main(){
 
@@ -28,7 +62,7 @@ int main(){
     int linhas = contaLinhas(arq);
     printf("Número de linhas do arquivo: %d\n", linhas);
 
-    code *memoria = NULL;
+    instrucao *memoria = NULL;
     lerArquivo(arq, &memoria, linhas);
 
     printf("\n\n0 - Rodar automaticamente\n1 - Rodar passo a passo\n");
@@ -39,7 +73,7 @@ int main(){
     return 0;
 }
 
-int contaLinhas(char *arq){
+int contaLinhas(char *arq){      // Analisar se é necessário - Atualmente é redundante.
     arquivo = fopen(arq, "r");
     char ch;
     int count=0;
@@ -56,32 +90,35 @@ int contaLinhas(char *arq){
     return count;
 }
 
-void lerArquivo(char *arq, code **memoria, int linhas){
-    *memoria = (code *)malloc(256 * sizeof(code));
+void lerArquivo(char *arq, instrucao **memoria, int linhas){
+    *memoria = (instrucao *)malloc(256 * sizeof(instrucao));
     arquivo = fopen(arq, "r");
     int i=0;
     char mem[17];
+    
     if(arquivo==NULL){
         printf("Permissão negada!");
         return;
     }
-    else{
-        while(i < linhas && fscanf(arquivo, "%16s", mem) != EOF){
-            strcpy((*memoria)[i].mem, mem);
-            printf("%dª memória: %s\n", i+1, (*memoria)[i].mem);
-            i++;
-        }
+    while(i < linhas && fscanf(arquivo, "%16s", mem) != EOF){
+        
+        strcpy((*memoria)[i].mem, mem);
+        printf("%dª memória: %s\n", i+1, (*memoria)[i].mem);
+        
+        (*memoria)[i].instrucao = strtoul(mem, NULL, 2);    // Transforma a string em uint16_t
+        i++;
     }
+    
     fclose(arquivo);
 }
 
-void programCounter(code *memoria, int linhas, int esc){
+void programCounter(instrucao *memoria, int linhas, int esc){
 
     int pc = 0;
 
     while(pc < linhas){
         printf("\nPC = %d | Memória = %s\n", pc, memoria[pc].mem);
-
+        decodificaInst(&memoria[pc]);
 
         if(esc == 1){
             printf("Pressione enter para o próximo passo\n\n");
@@ -89,4 +126,14 @@ void programCounter(code *memoria, int linhas, int esc){
         }
         pc++;
     }
+}
+
+void decodificaInst(instrucao *instrucao){
+
+    (*instrucao).opcode = (*instrucao).instrucao >> 12;
+
+    printf("opcode: %d\n", (*instrucao).opcode);
+
+
+
 }
