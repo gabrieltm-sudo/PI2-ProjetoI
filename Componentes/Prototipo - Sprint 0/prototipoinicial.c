02a5fc308f27613684b989ac5ffada35b6906fc2
AlexMaronVaz <alexmaronvaz@gmail.com>
2026-03-26 14:20:19 -0300

    Modificado Banco de Registradores e Protótipos

--- Componentes/Prototipo - Sprint 0/prototipoinicial.c
+++ Componentes/Prototipo - Sprint 0/prototipoinicial.c
@@ -1,5 +1,3 @@
-// Deve liberar reg
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -58,35 +56,59 @@ opcode: 4 bits;
 End: 8 bits;
 */
 
+// ------------------------------ PROTÓTIPOS -------------------------------
+
+// MENU / CONTROLE DO SISTEMA
+void imprimeSimulador(); // (não implementado)
+void salvaASM();         // (não implementado)
+void salvaDAT();         // (não implementado)
+void executaPrograma();  // (não implementado)
+void executaInstrucao(); // (não implementado - execução ainda não faz parte)
+void voltaInstrucao();   // (não implementado)
+
+
+// MEMÓRIA
 int contaLinhas(char *arq);
-
-//************ Editar parâmetros conforme criação da função **************
-int *inicializaBReg();
-void gerenciarBReg(int *reg);
-void imprimeBancoRegistradores(int *reg);
-void imprimeSimulador();
-void salvaASM();
-void salvaDAT();
-void executaPrograma();
-void executaInstrucao();
-void voltaInstrucao();
-
-// Componentes
 void lerMem(char *arq, instrucao **memoria, int linhas);
-void programCounter(instrucao *memoria, int linhas, int esc);
-int buscaInstrucao();
-void decodificaInst(instrucao *instrucao); // OK
+
+
+// PROGRAM COUNTER (PC) / BUSCA
+void programCounter(instrucao *memoria, int linhas, int esc, int *bReg);
+int buscaInstrucao();     // (não implementado - busca já ocorre via PC)
+
+
+// DECODIFICAÇÃO
+void decodificaInst(instrucao *instrucao);
+
+
+// UNIDADE DE CONTROLE (UC)
 void unidadeControle(instrucao *instrucao, sinaisUC *sinais);
-void executaInstrucaoR();
-void executaInstrucaoI();
-void executaInstrucaoJ();
-void ULA();
-void atualizaPC();
+
+
+// BANCO DE REGISTRADORES (BREG)
+int *inicializaBReg();
+void lerRegistradores(int *reg, int rs, int rt, int *valRs, int *valRt);
+void escreveRegistrador(int *reg, int rd, int valor, int EscReg);
+void imprimeBancoRegistradores(int *reg);
+
+
+// EXECUÇÃO (AINDA NÃO IMPLEMENTADA)
+void executaInstrucaoR(); // (não implementado)
+void executaInstrucaoI(); // (não implementado)
+void executaInstrucaoJ(); // (não implementado)
+
+
+// ULA (UNIDADE LÓGICA E ARITMÉTICA)
+void ULA();               // (não implementado)
+
+// -------------------------------------------------------------------------
 
 int main(){
 
     int esc;
     int opcao;
+    instrucao *memoria = NULL;
+    int linhas=0;
 
     int *bReg = inicializaBReg();
 
@@ -103,6 +125,7 @@ int main(){
         printf("9. Volta uma instrução (back)\n");
         printf("0. Sair\n\n");
         printf("Digite uma opção: ");
+
         scanf("%d", &opcao);
         getchar();
 
@@ -111,53 +134,66 @@ int main(){
                 //Carregar Mem de Instr.
                 char arq[20];
                 printf("\nDigite o nome do arquivo de memória: ");
+
                 fgets(arq, sizeof(arq), stdin);
                 arq[strcspn(arq, "\n")] = '\0';
 
-                int linhas = contaLinhas(arq);
+                linhas = contaLinhas(arq);
                 printf("\nNúmero de linhas do arquivo: %d\n", linhas);
 
-                instrucao *memoria = NULL;
                 lerMem(arq, &memoria, linhas);
                 break;
+
             case 2:
                 //Carregar Mem de Dados
                 break;
+
             case 3:
                 //Imprimir Banco de Registradores
                 imprimeBancoRegistradores(bReg);
                 break;
 
+
             case 4:
                 //Imprimir simulador
                 break;
+
             case 5:
                 // Salvar .asm
                 break;
+
             case 6:
                 // Salvar .dat
                 break;
+
             case 7:
                 //Executar programa (run)
                 esc = 0;
-                programCounter(memoria, linhas, esc);
+                programCounter(memoria, linhas, esc, bReg);
                 break;
+
             case 8:
                 //Executa instrução (step)
                 esc = 1;
-                programCounter(memoria, linhas, esc);
+                programCounter(memoria, linhas, esc, bReg);
                 break;
+
             case 9:
                 //Voltar instrução (back)
                 break;
+
             case 0:
                 //Sair
+                free(bReg);
                 free(memoria);
                 return 0;
+
             default:
                 printf("\nOpção inválida!\n");
+                break;
         }
     }
+
     return 0;
 }
 
@@ -165,15 +201,18 @@ int contaLinhas(char *arq){      // Analisar se é necessário - Atualmente é r
     arquivo = fopen(arq, "r");
     char ch;
     int count=0;
+
     if(arquivo==NULL){
         printf("\nAcesso negado!\n");
         return 0;
     }
+
     while((ch=fgetc(arquivo))!=EOF){
         if(ch=='\n'){
             count++;
         }
     }
+
     fclose(arquivo);
     return count;
 }
@@ -189,7 +228,9 @@ void lerMem(char *arq, instrucao **memoria, int linhas){
         printf("\nPermissão negada!");
         return;
     }
+
     printf("\nMemória de Instruções\n\n");
+
     while(i < linhas && fscanf(arquivo, "%16s", mem) != EOF){
         
         strcpy((*memoria)[i].mem, mem);
@@ -203,20 +244,23 @@ void lerMem(char *arq, instrucao **memoria, int linhas){
 }
 
 // PC
-void programCounter(instrucao *memoria, int linhas, int esc){
+void programCounter(instrucao *memoria, int linhas, int esc, int *bReg){
 
     int pc = 0;
 
     while(pc < linhas){ // Clock
 
         printf("\nPC = %d | Memória = %s\n", pc, memoria[pc].mem);
+
         decodificaInst(&memoria[pc]);
         unidadeControle(&memoria[pc], &sinais);
 
         if(esc == 1){
             printf("Pressione enter para o próximo passo\n\n");
+
             getchar();
         }
+
         pc++;
     }
 }
@@ -242,12 +286,14 @@ void decodificaInst(instrucao *instrucao){
         printf("rd: %d\n", (*instrucao).rd);
         printf("funct: %d\n", (*instrucao).funct);
         break;
+
     case 2:
         (*instrucao).tipoInst = tipoJ;
         (*instrucao).addr = ((*instrucao).instrucao) &0xFF; // pega os 8 bits do adress
         printf("[ Tipo J ]\n");
         printf("adress: %d\n", (*instrucao).addr);
         break;
+
     default:
         (*instrucao).tipoInst = tipoI;
         (*instrucao).rs = ((*instrucao).instrucao >> 9) &0x7; // pega os 3 bits do rs
@@ -283,10 +329,12 @@ void unidadeControle(instrucao *instrucao, sinaisUC *sinais){
                 // chama ULA e o que mais for necessário.
             }
             break;
+
         case 2:
             (*sinais).jump = 1;
             printf("\nlógica jump\n");
             break;
+
         case 4: // opcode = 0100 - Addi
             (*sinais).RegDst = 0;
             (*sinais).EscReg = 1;
@@ -299,6 +347,7 @@ void unidadeControle(instrucao *instrucao, sinaisUC *sinais){
 
             printf("\nAddi $%d, $%d, %d\n", (*instrucao).rs, (*instrucao).rt, (*instrucao).imm);
             break;
+
         case 8: // opcode = 1000 - BEQ
             (*sinais).RegDst = 0;
             (*sinais).EscReg = 0;
@@ -310,7 +359,9 @@ void unidadeControle(instrucao *instrucao, sinaisUC *sinais){
             (*sinais).branch = 1; // verificar também
 
             printf("\nbeq $%d, $%d, %d\n", (*instrucao).rs, (*instrucao).rt, (*instrucao).imm);
+
             break;
+
         case 11: // opcode = 1011 - lw
             (*sinais).RegDst = 0;
             (*sinais).EscReg = 1;
@@ -322,7 +373,9 @@ void unidadeControle(instrucao *instrucao, sinaisUC *sinais){
             (*sinais).branch = 0;
             
             printf("\nlw $%d, $%d, %d\n", (*instrucao).rs, (*instrucao).rs, (*instrucao).imm);
+
             break;
+
         case 15: // opcode = 1111 - sw
             (*sinais).RegDst = 0;
             (*sinais).EscReg = 0;
@@ -334,6 +387,7 @@ void unidadeControle(instrucao *instrucao, sinaisUC *sinais){
             (*sinais).branch = 0;
 
             printf("\nsw $%d, $%d, %d\n", (*instrucao).rs, (*instrucao).rt, (*instrucao).imm);
+
             break;
     }
 
@@ -351,22 +405,20 @@ int *inicializaBReg(){
     return reg;
 }
 
-// gerenciar BREG (?)
-void gerenciarBReg(int *reg){
-    int altReg, tempReg;
-
-    printf("Qual registrador deseja alterar? (0-7)");
-    scanf("%d", &altReg);
-
-    printf("Qual o novo valor? ");
-    scanf("%d", &tempReg);
-
-    reg[altReg] = tempReg;
+void lerRegistradores(int *reg, int rs, int rt, int *valRs, int *valRt){
+    *valRs = reg[rs];
+    *valRt = reg[rt];
 }
 
-// ?
+void escreveRegistrador(int *reg, int rd, int valor, int EscReg){
+    if(EscReg){
+        reg[rd] = valor;
+    }
+}
+
 void imprimeBancoRegistradores(int *reg){
-    printf("\nBanco de Registradores\n\n");
+    printf("\nBanco de Registradores:\n\n");
+
     for(int i=0;i<8;i++){
         printf("Registrador %d: %d\n",i, reg[i]);
     }
